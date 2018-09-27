0a661fd8a0214e1b5a16ebf5d64888b6dcb80f83
sergey-raevskiy <ccik@inbox.ru>
2018-09-27 13:24:41 +0300

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GoliathTalkBack
+{
+    static class Program
+    {
+        private static NotifyIcon ic = new NotifyIcon();
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            ic = new NotifyIcon();
+
+            Application.Run();
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main2()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
